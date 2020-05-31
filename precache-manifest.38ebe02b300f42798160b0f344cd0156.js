1c0cf45f4b77908c8410a19f597b9c49b607fc42
kudas g <kudosdesigner@hotmail.com>
2020-05-30 22:49:52 -0400

    Updates

---
+++ precache-manifest.38ebe02b300f42798160b0f344cd0156.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f384f37f6e8d82e25c4656e054e61512",
+    "url": "/projectprofile/index.html"
+  },
+  {
+    "revision": "8b32995424fd12d38de7",
+    "url": "/projectprofile/static/css/main.cd2f2369.chunk.css"
+  },
+  {
+    "revision": "08db67a3b1c45bb2c844",
+    "url": "/projectprofile/static/js/2.bc3fe26f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projectprofile/static/js/2.bc3fe26f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b32995424fd12d38de7",
+    "url": "/projectprofile/static/js/main.5a0a3f4a.chunk.js"
+  },
+  {
+    "revision": "bd10554fe47ed205833c",
+    "url": "/projectprofile/static/js/runtime-main.fd6ff74e.js"
+  }
+]);
